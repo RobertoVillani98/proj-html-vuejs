004d961728f73000039cf30278700183ee8a33e8
RobertoVillani <robertovillani58@gmail.com>
2022-01-11 16:44:06 +0100

    header

--- src/components/macro/Header.vue
+++ src/components/macro/Header.vue
@@ -1,5 +1,27 @@
 <template>
-  <header></header>
+  <header>
+    <div class="container">
+      <nav>
+        <img src="../../assets/img/light-logo.png" alt="max choach logo" />
+        <ul>
+          <li>Home</li>
+          <li>Pages</li>
+          <li>Courses</li>
+          <li>Features</li>
+          <li>Blog</li>
+          <li>Shop</li>
+          <li><i class="fas fa-shopping-cart"></i></li>
+          <li><i class="far fa-user-circle"></i></li>
+          <li>
+            <div class="ricerca">
+              <input class="search-bar" type="text" placeholder="Search..." />
+              <i id="search" class="fas fa-search"></i>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -9,4 +31,50 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+header {
+  color: white;
+}
+.container {
+  height: 60px;
+  background-color: #182154;
+  line-height: 60px;
+
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    img {
+      width: 150px;
+      margin-right: 100px;
+    }
+  }
+  ul {
+    display: flex;
+    align-items: center;
+  }
+  li {
+    list-style: none;
+    padding: 0px 15px;
+    font-size: 14px;
+    cursor: pointer;
+  }
+
+  .ricerca {
+    position: relative;
+  }
+  .search-bar {
+    height: 35px;
+    width: 150px;
+    border: none;
+    border-radius: 5px;
+    padding: 10px;
+  }
+  #search {
+    position: absolute;
+    left: 120px;
+    top: 24px;
+    color: #1fad96;
+  }
+}
 </style>
+ 
