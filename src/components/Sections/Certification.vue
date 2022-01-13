a2e06c7a3240908b60448345a5926a3d735d37e4
RobertoVillani <robertovillani58@gmail.com>
2022-01-13 11:54:36 +0100

    add section

---
+++ src/components/Sections/Certification.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="cert">
+    <div class="cert-subtitle">
+      Start today for getting <span>Online Certification</span>
+    </div>
+    <div class="cert-title">
+      You can be your own guiding star with our help!
+    </div>
+    <Button :label="buttonLabel2" />
+  </div>
+</template>
+
+<script>
+import Button from "../button/Button.vue";
+
+export default {
+  components: { Button },
+
+  name: "Certification",
+  data() {
+    return {
+      buttonLabel2: "Get started for free",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.cert {
+  text-align: center;
+  padding: 50px 0;
+  &-subtitle {
+    font-size: 20px;
+    margin-bottom: 10px;
+
+    span {
+      color: #27ab94;
+    }
+  }
+  &-title {
+    font-size: 30px;
+  }
+}
+</style>
---
+++ src/components/Sections/Help.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="helps">
+    <div class="help">
+      <div class="help-subtitle">wanna transform your life</div>
+      <div class="help-title">Glad to <span>Help You</span> Learn</div>
+    </div>
+
+    <div class="row">
+      <div class="card">
+        <i class="far fa-flag"></i>
+        <div class="card-text">
+          <div class="title">
+            <p>Idea Discussion</p>
+          </div>
+          <div class="desc">
+            <p>
+              Get teamed up with the specialists who work and teach coding for
+              years at famous universities.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <i class="fas fa-cog"></i>
+        <div class="card-text">
+          <div class="title">
+            <p>Web Development</p>
+          </div>
+          <div class="desc">
+            <p>
+              Learn to start building a webpages from scratch. You decide your
+              own pace, cuorse and speed.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <i class="far fa-life-ring"></i>
+        <div class="card-text">
+          <div class="title">
+            <p>System Administration</p>
+          </div>
+          <div class="desc">
+            <p>
+              Learners are encouraged to study the mechanism and structure of
+              system administration.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <i class="fas fa-desktop"></i>
+        <div class="card-text">
+          <div class="title">
+            <p>Graphic Design</p>
+          </div>
+          <div class="desc">
+            <p>
+              Have a passion for graphics and arts? Show your talents with
+              confidence and self-asssertiveness.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="image">
+        <img src="../../assets/img/home-6-services-image-1024x411.png" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Help",
+};
+</script>
+
+<style lang="scss" scoped>
+.helps {
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 1) 60%,
+    rgba(191, 191, 191, 1) 70%,
+    rgba(229, 229, 229, 1) 80%,
+    rgba(255, 255, 255) 90%
+  );
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  .help {
+    text-align: center;
+    padding: 80px 0;
+
+    &-subtitle {
+      text-transform: uppercase;
+      color: grey;
+      font-size: 13px;
+      font-weight: 200;
+    }
+
+    &-title {
+      font-size: 40px;
+      margin: 20px 0;
+      font-weight: 400;
+
+      span {
+        color: #27ab94;
+      }
+    }
+  }
+  .row {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    margin: 0 250px;
+    gap: 10px;
+  }
+
+  .card {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .fa-flag {
+    font-size: 50px;
+  }
+
+  .fa-cog {
+    font-size: 50px;
+  }
+
+  .fa-life-ring {
+    font-size: 50px;
+  }
+
+  .fa-desktop {
+    font-size: 50px;
+  }
+
+  .card-text {
+    margin-top: 20px;
+
+    .title {
+      font-size: 18px;
+    }
+
+    .desc {
+      font-size: 14px;
+      color: grey;
+      margin: 10px 0;
+      line-height: 1.6;
+      font-weight: 200;
+      width: 80%;
+    }
+  }
+
+  .image img {
+    width: 250%;
+    margin: 10px 110px;
+  }
+}
+</style>
